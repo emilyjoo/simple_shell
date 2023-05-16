aad58bcabf929b5299bceba3a70083a1836870bc
abdul-09 <abdul.9.hussein@gmail.com>
2023-05-16 01:02:47 -0700

    shell

---
+++ our_shell.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * @ac: arguments count
+ * @av: argument vector
+ * @env: environment variable
+ * Return: always 0
+ */
+
+int main(int ac, char **av, char **env)
+{
+	if (ac == 1)
+		prompt(av, env);
+	return (0);
+}
