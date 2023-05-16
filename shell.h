b930d5abdd82000cc65805165fbe8b8d2f595eec
Fatima Zahra Sabri <fzsabri3@gmail.com>
2023-05-16 12:35:04 -0700

    header file

--- shell.h
+++ shell.h
@@ -1,11 +1,28 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
-#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
 
 void prompt(char **av, char **env);
+
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
 #endif
