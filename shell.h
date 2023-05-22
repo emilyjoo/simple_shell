08c0a335e06bec6fef2ed3205a1add5cf43a831b
abdul-09 <abdul.9.hussein@gmail.com>
2023-05-22 05:16:58 -0700

    header file

--- shell.h
+++ shell.h
@@ -59,15 +59,22 @@ typedef struct alias_s
 /* Global aliases linked list */
 alias_t *aliases;
 
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char **_strtok(char *line, char *delim);
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strcpy(char *dest, const char *src);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream);
+void *re_alloc(void *ptr, unsigned int past_size, unsigned int current_size);
+char **str_tok(char *line, char *delim);
 char *get_location(char *command);
 list_t *get_path_dir(char *path);
 int execute(char **args, char **front);
 void free_list(list_t *head);
 char *_itoa(int num);
-
 
 void handle_line(char **line, ssize_t read);
 void variable_replacement(char **args, int *exe_ret);
@@ -79,32 +86,7 @@ int check_args(char **args);
 void free_args(char **args, char **front);
 char **replace_aliases(char **args);
 
-/* String functions */
-int _strlen(const char *s);
-char *_strcat(char *dest, const char *src);
-char *_strncat(char *dest, const char *src, size_t n);
-char *_strcpy(char *dest, const char *src);
-char *_strchr(char *s, char c);
-int _strspn(char *s, char *accept);
-int _strcmp(char *s1, char *s2);
-int _strncmp(const char *s1, const char *s2, size_t n);
-
-
-int (*get_builtin(char *command))(char **args, char **front);
-int shellby_exit(char **args, char **front);
-int shellby_env(char **args, char **front);
-int shellby_setenv(char **args, char **front);
-int shellby_unsetenv(char **args, char **front);
-int shellby_cd(char **args, char **front);
-int shellby_alias(char **args, char **front);
-int shellby_help(char **args, char **front);
-
-
-char **_copyenv(void);
-void free_env(void);
-char **_getenv(const char *var);
-
-
+/* Error Handling */
 int create_error(char **args, int err);
 char *error_env(char **args);
 char *error_1(char **args);
@@ -114,11 +96,22 @@ char *error_2_syntax(char **args);
 char *error_126(char **args);
 char *error_127(char **args);
 
-
 alias_t *add_alias_end(alias_t **head, char *name, char *value);
 void free_alias_list(alias_t *head);
 list_t *add_node_end(list_t **head, char *dir);
 void free_list(list_t *head);
+
+int (*get_builtin(char *command))(char **args, char **front);
+int shell_exit(char **args, char **front);
+int env_cmd(char **args, char **front);
+int setenv_cmd(char **args, char **front);
+int unsetenv_cmd(char **args, char **front);
+int cd_cmd(char **args, char **front);
+int alias_cmd(char **args, char **front);
+int shell_help(char **args, char **front);
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
 
 void help_all(void);
 void help_alias(void);
